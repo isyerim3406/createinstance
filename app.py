dfeaa338d08bf4f451f285d7dfacba5d324a9e64
isyerim3406 <isyerim3406@gmail.com>
2025-10-01 10:54:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 from flask import Flask
 from dotenv import load_dotenv
 import oci
@@ -8,13 +9,19 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# Oracle SDK için env değişkenlerini kullan
+# Private key’i temp dosyaya yaz
+private_key_content = os.getenv("OCI_PRIVATE_KEY")
+with tempfile.NamedTemporaryFile(delete=False, mode="w") as f:
+    f.write(private_key_content)
+    key_file_path = f.name
+
+# Oracle SDK config
 config = {
     "user": os.getenv("OCI_USER"),
     "fingerprint": os.getenv("OCI_FINGERPRINT"),
     "tenancy": os.getenv("OCI_TENANCY"),
     "region": os.getenv("OCI_REGION"),
-    "key_file": os.getenv("OCI_PRIVATE_KEY_PATH", "oci_api_key.pem")
+    "key_file": key_file_path
 }
 
 @app.route("/")
